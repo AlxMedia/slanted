1ac609fbb90992fe33b6338c738d8109f647bb00
Alexander Agnarson <alexander.agnarson@gmail.com>
2017-12-17 14:03:52 +0100

    Replace Google+ sharrre with LinkedIn, get rid of curl theme check errors

--- inc/sharrre.php
+++ inc/sharrre.php
@@ -1,8 +1,8 @@
 <div class="sharrre-container group">
 	<div id="twitter" data-url="<?php echo the_permalink(); ?>" data-text="<?php echo the_title(); ?>" data-title="<?php esc_html_e('Tweet', 'slanted'); ?>"></div>
 	<div id="facebook" data-url="<?php echo the_permalink(); ?>" data-text="<?php echo the_title(); ?>" data-title="<?php esc_html_e('Like', 'slanted'); ?>"></div>
-	<div id="googleplus" data-url="<?php echo the_permalink(); ?>" data-text="<?php echo the_title(); ?>" data-title="<?php esc_html_e('+1', 'slanted'); ?>"></div>
 	<div id="pinterest" data-url="<?php echo the_permalink(); ?>" data-text="<?php echo the_title(); ?>" data-title="<?php esc_html_e('Pin It', 'slanted'); ?>"></div>
+	<div id="linkedin" data-url="<?php echo the_permalink(); ?>" data-text="<?php echo the_title(); ?>" data-title="<?php esc_html_e('Share on LinkedIn', 'slanted'); ?>"></div>
 </div><!--/.sharrre-container-->
 
 <script type="text/javascript">
@@ -34,25 +34,11 @@
 				api.openPopup('facebook');
 			}
 		});
-		jQuery('#googleplus').sharrre({
-			share: {
-				googlePlus: true
-			},
-			template: '<a class="box group" href="#"><div class="count" href="#">{total}</div><div class="share"><i class="fa fa-google-plus-square"></i></div></a>',
-			enableHover: false,
-			enableTracking: true,
-			buttons:{size: 'tall'},
-			urlCurl: '<?php echo get_template_directory_uri() .'/js/sharrre.php'; ?>',
-			click: function(api, options){
-				api.simulateClick();
-				api.openPopup('googlePlus');
-			}
-		});
 		jQuery('#pinterest').sharrre({
 			share: {
 				pinterest: true
 			},
-			template: '<a class="box group" href="#" rel="nofollow"><div class="count" href="#">{total}</div><div class="share"><i class="fa fa-pinterest"></i></div></a>',
+			template: '<a class="box group" href="#"><div class="count" href="#">{total}</div><div class="share"><i class="fa fa-pinterest"></i></div></a>',
 			enableHover: false,
 			enableTracking: true,
 			buttons: {
@@ -63,6 +49,23 @@
 			click: function(api, options){
 				api.simulateClick();
 				api.openPopup('pinterest');
+			}
+		});
+		jQuery('#linkedin').sharrre({
+			share: {
+				linkedin: true
+			},
+			template: '<a class="box group" href="#"><div class="count" href="#">{total}</div><div class="share"><i class="fa fa-linkedin-square"></i></div></a>',
+			enableHover: false,
+			enableTracking: true,
+			buttons: {
+			linkedin: {
+				description: '<?php echo the_title(); ?>'<?php if( has_post_thumbnail() ){ ?>,media: '<?php echo wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>'<?php } ?>
+				}
+			},
+			click: function(api, options){
+				api.simulateClick();
+				api.openPopup('linkedin');
 			}
 		});
 	});
