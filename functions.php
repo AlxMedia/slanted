d53971e2f6c3c5263b023409d2b82572f3964a14
Alexander Agnarson <alexander.agnarson@gmail.com>
2018-07-20 18:03:42 +0200

    Remove custom.css option as it already exist in core

--- functions.php
+++ functions.php
@@ -185,7 +185,6 @@ if ( ! function_exists( 'slanted_styles' ) ) {
 		wp_enqueue_style( 'slanted-style', get_stylesheet_uri() );
 		if ( get_theme_mod('responsive','on') =='on' ) { wp_enqueue_style( 'slanted-responsive', get_template_directory_uri().'/responsive.css' ); }
 		if ( get_theme_mod('dark','off') == 'on' ) { wp_enqueue_style( 'slanted-dark', get_template_directory_uri().'/dark.css' ); }
-		if ( get_theme_mod('custom','off') == 'on' ) { wp_enqueue_style( 'slanted-custom', get_template_directory_uri().'/custom.css' ); }
 		wp_enqueue_style( 'slanted-font-awesome', get_template_directory_uri().'/fonts/font-awesome.min.css' );
 	}
 	
--- functions/theme-options.php
+++ functions/theme-options.php
@@ -63,15 +63,6 @@ Kirki::add_section( 'styling', array(
 /*  Add Fields
 /* ------------------------------------ */
 
-// General: Custom CSS
-Kirki::add_field( 'slanted_theme', array(
-	'type'			=> 'switch',
-	'settings'		=> 'custom',
-	'label'			=> esc_attr__( 'Custom Stylesheet', 'slanted' ),
-	'description'	=> esc_attr__( 'Load custom stylesheet (custom.css)', 'slanted' ),
-	'section'		=> 'general',
-	'default'		=> 'off',
-) );
 // General: Responsive Layout
 Kirki::add_field( 'slanted_theme', array(
 	'type'			=> 'switch',
